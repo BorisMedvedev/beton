39097eb4257be4aa228cb25e8401d73678ad7551
Your Name <you@example.com>
2022-07-14 11:36:38 +0300

    555

--- app/js/main.js
+++ app/js/main.js
@@ -298,24 +298,3 @@ btn.on('click', function(e) {
 
 
 
-const cookieEl = document.querySelector('.cookie-block');
-const okEl = document.querySelector('.ok');
-
-okEl.addEventListener('click', () => {
-    cookieEl.style.display = 'none';
-});
-
-let cookies = () => {
-    if (!Cookies.get('hide-cookie')) {
-        setTimeout(() => {
-            cookieEl.style.display = 'block';
-        }, 5000);
-    }
-
-    Cookies.set('hide-cookie', 'true', {
-        expires: 50
-    });
-}
-
-
-cookies();
